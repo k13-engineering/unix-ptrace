f3e29212eb50f2bc9e3c0acc5aae10a298323beb
Simon Kadisch <simon.kadisch@k13-engineering.at>
2021-04-26 23:11:24 +0200

    Added ESLint and fixed linter errors

--- examples/trace.js
+++ examples/trace.js
@@ -2,30 +2,34 @@
 
 const ptrace = require("../index.js");
 
-const main = async() => {
+const spawnAndLog = async (...args) => {
+  const proc = ptrace.spawn(...args);
+
+  let state = "leave";
+  let status = await proc.wait();
+
+  while (!status.exited()) {
+    const r = await proc.regs();
+    if (state === "enter") {
+      process.stdout.write(`\rsyscall ${r.orig_rax}\r`);
+    } else {
+      process.stdout.write(`\rsyscall ${r.orig_rax}, res = ${r.rax}\n`);
+    }
+
+    proc.syscall();
+    state = state === "enter" ? "leave" : "enter";
+
+    status = await proc.wait();
+  }
+
+  if (state === "leave") {
+    process.stdout.write("\n");
+  }
+};
+
+const main = async () => {
   try {
-    const proc = ptrace.spawn("/bin/echo", ["Hello", "World!"]);
-
-    let state = "leave";
-    let status = await proc.wait();
-
-    while (!status.exited()) {
-      const r = await proc.regs();
-      if (state === "enter") {
-        process.stdout.write("\rsyscall " + r.orig_rax + "\r");
-      } else {
-        process.stdout.write("\rsyscall " + r.orig_rax + ", res = " + r.rax + "\n");
-      }
-
-      proc.syscall();
-      state = state === "enter" ? "leave" : "enter";
-
-      status = await proc.wait();
-    }
-
-    if (state === "leave") {
-      process.stdout.write("\n");
-    }
+    await spawnAndLog("/bin/echo", ["Hello", "World!"]);
   } catch (ex) {
     console.error(ex);
   }
--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const child_process = require("child_process");
-
-const ref = require("ref-napi");
 const Struct = require("ref-struct-napi");
 
 const unix = require("./lib/unix");
@@ -36,27 +33,19 @@ const UserRegs = Struct({
   "gs": "int64",
 });
 
-const PTRACE_TRACEME = 0
-const PTRACE_PEEKTEXT = 1
-const PTRACE_PEEKDATA = 2
-const PTRACE_PEEKUSR = 3
-const PTRACE_POKETEXT = 4
-const PTRACE_POKEDATA = 5
-const PTRACE_POKEUSR = 6
-const PTRACE_CONT = 7
-const PTRACE_KILL = 8
-const PTRACE_SINGLESTEP = 9
+const PTRACE_TRACEME = 0;
+const PTRACE_CONT = 7;
+const PTRACE_SINGLESTEP = 9;
 
-const PTRACE_GETREGS = 12
-const PTRACE_SETREGS = 13
+const PTRACE_GETREGS = 12;
+const PTRACE_SETREGS = 13;
 
-const PTRACE_ATTACH = 0x10
-const PTRACE_DETACH = 11
+const PTRACE_DETACH = 11;
 
-const PTRACE_SYSCALL = 24
+const PTRACE_SYSCALL = 24;
 
 module.exports = new function () {
-  this.spawn = (path, args, options) => {
+  this.spawn = (path, args) => {
     const pid = unix.fork();
     if (pid === 0) {
       try {
@@ -64,7 +53,7 @@ module.exports = new function () {
           const x = unix.dup(stream.fd);
           unix.dup2(x, fd);
           unix.close(x);
-        }
+        };
 
         _dup2(process.stdin, 0);
         _dup2(process.stdout, 1);
@@ -81,31 +70,32 @@ module.exports = new function () {
     const mem = memory.accessor(pid);
 
     return new function () {
-      this.wait = (options) => unix.waitpid(-1, options || 0);
+      this.wait = (opts) => unix.waitpid(-1, opts || 0);
       this.cont = () => unix.ptrace(PTRACE_CONT, pid, null, null);
       this.syscall = () => unix.ptrace(PTRACE_SYSCALL, pid, null, null);
       this.singlestep = () => unix.ptrace(PTRACE_SINGLESTEP, pid, null, null);
 
-      this.regs = async(r) => {
+      this.regs = async (r) => {
         if (r) {
-          let data = await this.regs();
-          for (let key in r) {
+          const data = await this.regs();
+          for (const key in r) {
             if (typeof data[key] === "undefined") {
-              throw new Error("unkown register '" + key + "'");
+              throw new Error(`unkown register '${key}'`);
             }
             data[key] = r[key];
           }
 
           unix.ptrace(PTRACE_SETREGS, pid, null, data.ref());
+          return data;
         } else {
-          let res = new UserRegs();
+          const res = new UserRegs();
           unix.ptrace(PTRACE_GETREGS, pid, null, res.ref());
           return res;
         }
       };
 
-      this.peek = async(offset, size) => await mem.peek(offset, size);
-      this.poke = async(offset, data) => await mem.poke(offset, data);
+      this.peek = async (offset, size) => await mem.peek(offset, size);
+      this.poke = async (offset, data) => await mem.poke(offset, data);
 
       this.detach = () => unix.ptrace(PTRACE_DETACH, pid, null, null);
     };
