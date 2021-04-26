6897ce0f6e502f61f0390cc9f554aab9e6d35db2
Simon Kadisch <simon.kadisch@k13-engineering.at>
2021-04-27 00:28:14 +0200

    Moved architecture-dependent structures out of index.js

--- index.js
+++ index.js
@@ -1,37 +1,7 @@
-import Struct from "ref-struct-napi";
-
 import unix from "./lib/unix.js";
 import memory from "./lib/memory.js";
 
-const UserRegs = Struct({
-  "r15": "int64",
-  "r14": "int64",
-  "r13": "int64",
-  "r12": "int64",
-  "rbp": "int64",
-  "rbx": "int64",
-  "r11": "int64",
-  "r10": "int64",
-  "r9": "int64",
-  "r8": "int64",
-  "rax": "int64",
-  "rcx": "int64",
-  "rdx": "int64",
-  "rsi": "int64",
-  "rdi": "int64",
-  "orig_rax": "int64",
-  "rip": "int64",
-  "cs": "int64",
-  "eflags": "int64",
-  "rsp": "int64",
-  "ss": "int64",
-  "fs_base": "int64",
-  "gs_base": "int64",
-  "ds": "int64",
-  "es": "int64",
-  "fs": "int64",
-  "gs": "int64",
-});
+import structureFactory from "./lib/arch/index.js";
 
 const PTRACE_TRACEME = 0;
 const PTRACE_CONT = 7;
@@ -70,6 +40,8 @@ const forkAndExecve = (path, args) => {
 };
 
 const spawn = async (path, args) => {
+  const { Registers } = structureFactory.create();
+
   const pid = forkAndExecve(path, args);
 
   const mem = memory.accessor(pid);
@@ -92,7 +64,7 @@ const spawn = async (path, args) => {
       unix.ptrace(PTRACE_SETREGS, pid, null, data.ref());
       return data;
     } else {
-      const res = UserRegs();
+      const res = Registers();
       unix.ptrace(PTRACE_GETREGS, pid, null, res.ref());
       return res;
     }
